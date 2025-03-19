a0ef5ce7b03f5c927723cd4c83a643c24a4a8d8f
divyaaaaa2002 <divyaachardivya581@gmail.com>
2025-03-19 15:05:18 +0530

    completed

--- backend/app/views.py
+++ backend/app/views.py
@@ -9,4 +9,10 @@ def index(request):
     temperature=api['main']['temp']
     country=api['sys']['country']
     city_name=api['name']
-    return render(request,"index.html",{'temperature':temperature,"country":country,"city":city_name})
+    speed=api['wind']['speed']
+    humidity=api['main']['humidity']
+    cloud=api['weather'][0]['description']
+    main=api['weather'][0]['main']
+    icon=api['weather'][0]['icon']
+    icon_url=f'https://openweathermap.org/img/wn/{icon}@2x.png'
+    return render(request,"index.html",{'temperature':temperature,"country":country,"city":city_name,"speed":speed,"humidity":humidity,"cloud":cloud,"main":main,"icon":icon_url})
